c95391de12424db7a14ee5060c3f9cc7f5141d3b
Jia Lu <lujiacn@gmail.com>
2019-09-07 15:42:40 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[derive(Debug)]
+struct Solution{}
+
+impl Solution {
+    //TODO;
+    fn add(a: i64, b:i64) -> i64 {
+        0        
+    }
+}
+
+#[test]
+fn test1() {
+    let b = Solution::add(1,3)
+    assert!(Solution::add(a:1, b:3), 4)
+    
+}
+
+
