66537e501b82263b055c307c4b4554bd07be70c4
Jia Lu <lujiacn@gmail.com>
2019-09-07 16:03:54 +0800

    added sub

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,24 @@ struct Solution {}
 impl Solution {
     //TODO:
     fn add(a: i64, b: i64) -> i64 {
-        0
+        a + b
+    }
+
+    fn sub(a: i64, b: i64) -> i64 {
+        a - b
     }
 }
 
 #[test]
 fn test1() {
     assert_eq!(Solution::add(1, 3), 4);
-    assert_eq!(Solution::add(7, 9), 6);
+    assert_ne!(Solution::add(7, 9), 6);
+}
+
+#[test]
+fn test2() {
+    assert_eq!(Solution::sub(3, 2), 1);
+    assert_ne!(Solution::sub(7, 9), 6);
 }
 
 fn main() {
